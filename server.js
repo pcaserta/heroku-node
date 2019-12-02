791d19cbe45aae081c6751beb2bec1686a8081b2
pcaserta <patrick.caserta@live.longwood.edu>
2019-12-02 16:55:31 -0500

    deploy

---
+++ server.js
@@ -0,0 +1,25 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/htmlRoutes.js")(app);
+
+app.listen(PORT, console.log(`Server started on ${PORT}`));
