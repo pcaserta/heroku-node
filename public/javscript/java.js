5604149fcc8fb610c33a6356e54ca143953a703e
pcaserta <patrick.caserta@live.longwood.edu>
2019-12-03 12:07:53 -0500

    psuedo code update

--- public/javscript/java.js
+++ public/javscript/java.js
@@ -2,18 +2,19 @@ $(document).ready(function() {
   let status = false;
   $("#submitLogin").on("click", function(event) {
     event.preventDefault();
-    //api call return true or false
+    //api-call should be here, setting default value to true for no
     status = true;
-
     let username = $("#userName")
       .val()
       .trim();
     localStorage.clear();
     localStorage.setItem("input", username);
     var localInput = localStorage.getItem("input");
+
+    //if logged in status is true and local input exists rederict to home page
     if (status && localInput) {
       window.location.href = "/afterLogin/index.html";
-    } else if (!status) {
+    } else {
       alert("Try again invalid username/password");
     }
   });
