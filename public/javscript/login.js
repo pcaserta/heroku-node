e9a114c47f930a8ff77570d3f7303e9fa8620335
pcaserta <patrick.caserta@live.longwood.edu>
2019-12-03 14:23:34 -0500

    validation to make sure user is logged in to view survey

--- public/javscript/login.js
+++ public/javscript/login.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   //sets username to navbar
   var localInput = localStorage.getItem("input");
   console.log(localInput);
+  if (localInput == null) {
+    window.location.href = "/landing/index.html";
+    alert("Please login to take survey");
+  }
   var name = $("<p>").text(`User ${localInput} logged in`);
   name.addClass("p-0 m-1");
   $("#NavLinks").append(name);
